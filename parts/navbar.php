a89ff1c1405e20233501157b9d0170b6a7f0da39
Ingo Gutwin <onmabu@gmail.com>
2025-08-17 22:10:24 +0200

    Refactor navigation menu

--- parts/navbar.php
+++ parts/navbar.php
@@ -1,17 +1,18 @@
 <?php
-$menu_items = wp_get_nav_menu_items('Navigation');
+$shooting_menu_items = wp_get_nav_menu_items('Shootings');
+$navigation_menu_items = wp_get_nav_menu_items('Navigation');
 ?>
-<nav class="navbar" id="navbar">
+<nav class="navbar">
   <div>
     <a href="<?php echo home_url() ?>">
       <div class="logo"><span class="-mb-3">authentische</span><br><span>Momente</span></div>
     </a>
   </div>
 
-  <div class="nav-right">
-    <button id="shootings-btn-nav" data-is-toggled="false">
+  <div class="nav-right" id="navbar-right">
+    <button id="shootings-toggle">
       shootings
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" aria-hidden="true" class="nav__submenu-toggle-icon">
+      <svg id="shootings-toggle-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none">
         <path d="M12 17L19 8H5L12 17Z" fill="currentColor"></path>
       </svg>
     </button>
@@ -23,10 +24,24 @@ $menu_items = wp_get_nav_menu_items('Navigation');
     </div>
 
     <ul
-      class="navigation"
-      id="navbar-links"
+      class="shootings"
+      id="shooting-links"
       data-is-toggled="false">
-      <?php foreach ($menu_items as $item) {
+      <?php foreach ($shooting_menu_items as $item) {
+        if ($item->post_title === 'home') {
+          continue;
+        } ?>
+        <li>
+          <a href="<?php echo $item->url; ?>"><?php echo $item->post_title; ?></a>
+        </li>
+      <?php } ?>
+    </ul>
+
+    <ul
+      class="navigation"
+      id="navigation-links"
+      data-is-toggled="false">
+      <?php foreach ($navigation_menu_items as $item) {
         if ($item->post_title === 'home') {
           continue;
         } ?>
