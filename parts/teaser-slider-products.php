751baece8c1b3ae01cc651df47a32518bbeb60ad
Ingo Gutwin <onmabu@gmail.com>
2025-08-18 13:59:45 +0200

    Add teaser-slider

---
+++ parts/teaser-slider-products.php
@@ -0,0 +1,16 @@
+<?php
+$teaser_slider_section = $args;
+
+echo '<pre>';
+var_dump($teaser_slider_section);
+echo '<pre>';
+?>
+<!-- teaser slider section start -->
+<section
+    class="product-teaser-slider-section"
+    id="product-teaser-slider">
+
+
+
+</section>
+<!-- teaser slider section start -->
