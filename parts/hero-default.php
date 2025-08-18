6ef5cbbab56f3d04a3ab545d3c1cbd724ad0bd3f
Ingo Gutwin <onmabu@gmail.com>
2025-08-18 14:00:18 +0200

    Fix array in hero-section

--- parts/hero-default.php
+++ parts/hero-default.php
@@ -9,6 +9,7 @@ $hero_section = $args;
 
 $hero_title       = $hero_section['hero_title'] ?? '';
 $hero_sub_title   = $hero_section['hero_sub_title'] ?? '';
+$hero_button_text = $hero_section['hero_button_text'] ?? '';
 $hero_bg_image_sm = $hero_section['hero_bg_image_sm'] . ' 558w' ?? '';
 $hero_bg_image_xl = $hero_section['hero_bg_image_xl'] . ' 1448w' ?? '';
 ?>
@@ -20,7 +21,7 @@ $hero_bg_image_xl = $hero_section['hero_bg_image_xl'] . ' 1448w' ?? '';
 		<img srcset="<?php echo esc_html( implode( ', ', array( $hero_bg_image_sm, $hero_bg_image_xl ) ) ); ?>" src="<?php echo esc_html( $hero_bg_image_xl ); ?>" alt="Schwangere Shooting in Bonn von authentische Momente" />
 	</picture>
 
-	<button class="book-appointment-btn" id="hero-btn-appointment"><?php echo esc_html( $hero_section['hero_button_text'] ) ?? ''; ?></button>
+	<button class="book-appointment-btn" id="hero-btn-appointment"><?php echo esc_html( $hero_button_text ); ?></button>
 		<h1><?php echo esc_html( $hero_title ); ?></h1>
 		<div class="hero-intro">
 		<?php echo wp_kses_post( $hero_sub_title ); ?>
