3e5ec0b2a6bf3bac6eea9c809cf847bab35ee971
Ingo Gutwin <onmabu@gmail.com>
2025-08-20 11:37:42 +0200

    Build teaser-slider html

--- parts/teaser-slider-products.php
+++ parts/teaser-slider-products.php
@@ -1,16 +1,33 @@
 <?php
-$teaser_slider_section = $args;
+/**
+ * Teaser Slider part for Products
+ *
+ * @package Theme authentische-momente
+ */
 
-echo '<pre>';
-var_dump($teaser_slider_section);
-echo '<pre>';
+$teaser_slider_data = $args;
 ?>
 <!-- teaser slider section start -->
-<section
-    class="product-teaser-slider-section"
-    id="product-teaser-slider">
-
-
-
+<section class="product-teaser-slider-section">
+	<div class="product-teaser-slider-container" id="product-teaser-slider-container">
+		<ul class="product-teaser-slider">
+			<?php
+			foreach ( $teaser_slider_data as $slide ) {
+				?>
+				<li class="slide">
+					<article>
+						<a href="<?php echo esc_html( $slide['url'] ); ?>">
+							<h2><?php echo esc_html( $slide['title'] ); ?></h2>
+							<p><?php echo esc_html( $slide['sub_title'] ); ?></p>
+						</a>
+					</article>
+					<picture>
+						<source type="image/webp" srcset="<?php echo esc_html( $slide['image'] ); ?>" />
+						<img src="<?php echo esc_html( $slide['image'] ); ?>" srcset="<?php echo esc_html( $slide['image'] . ' 558w' ); ?>" />
+					</picture>
+				</li>
+			<?php } ?>
+		</ul>
+	</div>
 </section>
 <!-- teaser slider section start -->
