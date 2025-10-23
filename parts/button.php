c4a8611b53af9ae7511212fbb83ab91b4fff95a2
Ingo Gutwin <onmabu@gmail.com>
2025-10-23 15:07:06 +0200

    Add url to contact button

--- parts/button.php
+++ parts/button.php
@@ -6,5 +6,6 @@
  */
 $css_class   = $args['css_class'];
 $button_text = $args['button_text'];
+$button_url  = $args['button_url'];
 ?>
 <button class="<?php echo esc_html( $css_class ); ?>"><?php echo esc_html( $button_text ); ?></button>
